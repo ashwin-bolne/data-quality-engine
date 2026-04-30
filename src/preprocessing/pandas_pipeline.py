a32b6882449703f5706e95164e0f27a9ab935094
Ashwin Bolne <ashwinbolne610cs@gmail.com>
2026-04-30 18:00:48 +0530

    feat: add drop_high_null_cols transformation with configurable thresold

--- src/preprocessing/pandas_pipeline.py
+++ src/preprocessing/pandas_pipeline.py
@@ -1,6 +1,17 @@
 import pandas as pd
 from typing import List, Callable
 
+def drop_high_nulls_cols(thresold: float = 0.5):
+    """
+    Returns a function that drops columns with null percentage above thresold
+    """
+    def _drop(df: pd.DataFrame) -> pd.DataFrame:
+        null_pct = df.isnull().mean()
+
+        cols_to_drop = null_pct[null_pct > thresold].index
+
+        return df.drop(columns=cols_to_drop)
+
 def run_pipeline(df: pd.DataFrame, steps: List[Callable[[pd.DataFrame], pd.DataFrame]]) -> pd.DataFrame:
     """
     Runs a sequence of transformation steps on the DataFrame.
